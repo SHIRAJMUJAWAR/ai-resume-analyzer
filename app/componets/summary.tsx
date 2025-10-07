ab0157afb0c1103934f0cdd189b15aa51f523e60
SHIRAJMUJAWAR <shirajmujawar03@gmail.com>
2025-10-07 23:05:38 +0530

    Fix: AI feedback model and add JSON parse error handling

--- app/componets/summary.tsx
+++ app/componets/summary.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import ScoreGauge from './ScoreGauge'
 
-const Summary = () => {
+const Category = ({title, score} : {title: string, score: number}) => {
+   const textColor = score >= 75 ? 'text-green-600' : score >= 50 ? 'text-yellow-600' : 'text-red-600';
+
+    return(
+        <div className='resume-summary'>
+            <div className='category'>
+                <div className='felx flex-row justify-between items-center'>
+                    <p className='text-2xl'> {title } - {score}</p>
+                </div>
+            </div> 
+        </div>
+    )
+}
+
+const Summary = ( {feedback} : {feedback : Feedback}) => {
   return (
-    <div>
-      summary
+    <div className='bg-white rounded-2xl shadow-md w-full'>
+       <div className='flex flex-row p-4 gap-8 items-center '>
+         <ScoreGauge score={feedback.overallScore}></ScoreGauge>
+
+         <div className='flex flex-col gap-2'>
+           <h2 className='text-2xl font-bold'>Your Resume Score</h2>
+           <p className='tetx-sm text-gray-600'>
+            This Score is calculated based on the variables listed below
+           </p>
+         </div>
+       </div>
+      <Category title='Tone & Style' score={feedback.toneAndStyle.score}/>
+      <Category title='Content' score={feedback.content.score}/>
+      <Category title='Structure' score={feedback.structure.score}/>
+      <Category title='Skills' score={feedback.skills.score}/>
+      
     </div>
   )
 }
